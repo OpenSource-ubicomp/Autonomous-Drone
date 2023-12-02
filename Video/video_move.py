8005e930f5f5fd54f13511111bef1f9d96b50df0
PCY00 <46348321+PCY00@users.noreply.github.com>
2023-12-02 13:03:38 +0900

    Update video_move.py

--- Video/video_move.py
+++ Video/video_move.py
@@ -2,28 +2,39 @@ import cv2
 import robomaster
 from robomaster import robot
 
-
+robomaster.config.LOCAL_IP_STR = "192.168.10.3"
 
 if __name__ == '__main__':
     tl_drone = robot.Drone()
     tl_drone.initialize()
 
+    tl_flight = tl_drone.flight
     tl_camera = tl_drone.camera
-    tl_camera.start_video_stream(display=False)  # 비디오 스트림 시작
+
+    # 비행 시작
+    tl_flight.takeoff().wait_for_completed()
+
+    # 비디오 스트림 시작
+    tl_camera.start_video_stream(display=False)
     tl_camera.set_fps("high")
     tl_camera.set_resolution("high")
     tl_camera.set_bitrate(6)
 
-    while True:
-        img = tl_camera.read_cv2_image()  # 비디오 프레임 읽어오기
-        cv2.imshow("Drone Video Stream", img)  # 영상 화면에 표시
+    # 전진 후진 수행
+    tl_flight.forward(distance=50).wait_for_completed()
+    tl_flight.backward(distance=50).wait_for_completed()
 
-        # 키보드 입력을 기다리고 'q'를 누르면 비디오 스트림 종료
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord('q'):
-            break
+    # 착륙
+    tl_flight.land().wait_for_completed()
 
-    cv2.destroyAllWindows()  # 모든 창 닫기
-    tl_camera.stop_video_stream()  # 비디오 스트림 중지
+    # 비디오 스트림 종료
+    tl_camera.stop_video_stream()
+
+    # 영상 스트림 확인 및 종료
+    for _ in range(302):
+        img = tl_camera.read_cv2_image()
+        cv2.imshow("Drone Video Stream", img)
+        cv2.waitKey(1)
+    cv2.destroyAllWindows()
 
     tl_drone.close()  # 드론 연결 종료
